bdb9a8d93b77dea621e4e06cdbac06498b79c16b
dwu04 <derekwu132@gmail.com>
2025-05-07 03:47:00 -0700

    testing branch: should be wrong

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Lab 5 - Starter
+
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
+
+**Derek Wu**
+
+1. No; unit tests should be used on isolated pieces of logic and is good for
+   small things but you may need more than one test for this since it's more
+   complicated.
+   <br>
+2. Yes; this is a small, isolated piece of logic that is easy to test.
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,56 @@
 // explore.js
 
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  // TODO
+  // voice
+  const voice_select = document.getElementById("voice-select");
+  const synth = window.speechSynthesis;
+
+  // button
+  const push_to_talk_btn = document.querySelector("button");
+  const speaking_img = document.querySelector("#explore img");
+  const input = document.querySelector("textarea");
+
+  // populate voices
+  let voices = [];
+  function populateVoicesList() {
+    voices = synth.getVoices();
+
+    voices.forEach((voice) => {
+      const option = document.createElement("option");
+
+      option.textContent = `${voice.name} (${voice.lang})`;
+      option.setAttribute("data-name", voice.name);
+
+      voice_select.appendChild(option);
+    });
+  }
+  populateVoicesList();
+
+  // on button press
+  push_to_talk_btn.addEventListener("click", () => {
+    const text = input.value;
+    const utterance = new SpeechSynthesisUtterance(text);
+    const selected_voice =
+      voice_select.selectedOptions[0].getAttribute("data-name");
+
+    if (!text || voice_select.selectedOptions[0].disabled) {
+      return;
+    }
+
+    voices.forEach((voice) => {
+      if (voice.name === selected_voice) {
+        utterance.voice = voice;
+      }
+    });
+
+    speaking_img.src = "assets/images/smiling-open.png";
+
+    utterance.onend = () => {
+      speaking_img.src = "assets/images/smiling.png";
+    };
+
+    synth.speak(utterance);
+  });
 }
